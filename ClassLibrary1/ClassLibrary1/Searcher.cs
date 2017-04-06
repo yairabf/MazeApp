2e1910819de972f917724d9399a07295acb1b4de
maggy <yair>
2017-04-06 15:04:12 +0300

    Create interfaces of ISearchable and

---
+++ ClassLibrary1/ClassLibrary1/Searcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public abstract class Searcher : ISearcher
+    {
+        private MyPriorityQueue<State> openList;
+        private int evaluatedNodes;
+
+        public Searcher()
+        {
+            openList = new MyPriorityQueue<State>();
+            evaluatedNodes = 0;
+        }
+        protected State popOpenList() { evaluatedNodes++; return openList.poll(); }
+        // a property of openList public int OpenListSize { // it is a read-only property :) get { return openList.Count; } }
+        // ISearcher’s methods:
+        public virtual int getNumberOfNodesEvaluated() { return evaluatedNodes; }
+        public abstract Solution search(ISearchable searchable);
+    }
+}
