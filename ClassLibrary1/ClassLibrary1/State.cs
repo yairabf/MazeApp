46443921d88f096f76f793cccfb43dc24ff0d9a4
yair abramovits <yair.abramovits@live.biu.ac.il>
2017-04-13 15:36:13 +0300

    commit from jetbrain IDE "rider"

--- ClassLibrary1/ClassLibrary1/State.cs
+++ ClassLibrary1/ClassLibrary1/State.cs
@@ -8,8 +8,8 @@ namespace ClassLibrary1
 {
     public class State<T>
     {
-        private T state;    // the state represented by a string 
-        private double cost;     // cost to reach this state (set by a setter) 
+        private T state;    // the state represented by a string
+        private double cost;     // cost to reach this state (set by a setter)
         private State<T> cameFrom;  // the state we came from to this state (setter)
 
         public State(T state)    // CTOR 
@@ -41,6 +41,7 @@ namespace ClassLibrary1
         {
             return state.ToString();
         }
+
         public void setCameFrom(State<T> s)
         {
             this.cameFrom = s;
---
+++ ClassLibrary1/ClassLibrary1/TestSearchable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SearchAlgorithmLib;
+
+class TestSearchable<T> : ISearchable<T>
+{
+    private State<T> from, to;
+    Dictionary<State<T>, List<State<T>>> Adj;
+
+    public TestSearchable(State<T> from, State<T> to, Dictionary<State<T>, List<State<T>>> Adj)
+    {
+        this.from = from;
+        this.to = to;
+        this.Adj = Adj;
+    }
+
+    public State<T> GetInitialState()
+    {
+        return from;
+    }
+
+    public State<T> GetGoalState()
+    {
+        return to;
+    }
+
+    public List<State<T>> GetAllPossibleStates(State<T> s)
+    {
+        List<State<T>> states = null;
+        // if found the vertex
+        if (Adj.ContainsKey(s))
+        {
+            states = Adj[s];
+        }
+
+        // if vertex does not exist or no neighbors found
+        if (states == null)
+        {
+            states = new List<State<T>>();
+        }
+        return states;
+    }
+}
