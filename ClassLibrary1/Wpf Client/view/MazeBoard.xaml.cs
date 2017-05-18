fb929f61628ac81cbe1887c3836b2fa860f8a96e
yair abramovits <yair.abramovits@live.biu.ac.il>
2017-05-19 00:26:23 +0300

    add to hanani views

---
+++ ClassLibrary1/Wpf Client/view/MazeBoard.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MazeLib;
+
+namespace Wpf_Client.view
+{
+    /// <summary>
+    /// Interaction logic for MazeBoard.xaml
+    /// </summary>
+    public partial class MazeBoard : UserControl
+    {
+        public MazeBoard()
+        {
+            InitializeComponent();
+
+        }
+
+        public int RowsUC
+        {
+            get { return (int)GetValue(RowsUCProperty); }
+            set { SetValue(RowsUCProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Rows. This enables animation, styling,
+        public static readonly DependencyProperty RowsUCProperty =
+            DependencyProperty.Register("RowsUC", typeof(int), typeof(MazeBoard), new
+                PropertyMetadata(0));
+
+
+
+        public string Maze
+        {
+            get { return (string)GetValue(MazeProperty); }
+            set { SetValue(MazeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for stringMazeUC.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MazeProperty =
+            DependencyProperty.Register("Maze", typeof(string), typeof(MazeBoard));
+
+
+
+        public int ColsUC
+        {
+            get { return (int)GetValue(ColsUCProperty); }
+            set { SetValue(ColsUCProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ColsUCProperty =
+            DependencyProperty.Register("MyProperty", typeof(int), typeof(MazeBoard), new PropertyMetadata(0));
+
+
+
+        public int InitialPosUC
+        {
+            get { return (int)GetValue(InitialPosUCProperty); }
+            set { SetValue(InitialPosUCProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for InitialPosUC.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty InitialPosUCProperty =
+            DependencyProperty.Register("InitialPosUC", typeof(int), typeof(MazeBoard), new PropertyMetadata(0));
+
+
+
+        public int GoalPosUC
+        {
+            get { return (int)GetValue(GoalPosUCProperty); }
+            set { SetValue(GoalPosUCProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for GoalPosUC.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty GoalPosUCProperty =
+            DependencyProperty.Register("GoalPosUC", typeof(int), typeof(MazeBoard), new PropertyMetadata(0));
+
+
+
+        public void DrawMaze()
+        {
+            string maze = Maze;
+            for (int i = 0; i < ColsUC; i++)
+            {
+                
+            }
+        }
+
+    }
+}
---
+++ ClassLibrary1/Wpf Client/view/SinglePlayerMenu.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfMaze.model;
+using Wpf_Client.model;
+using Wpf_Client.viewModel;
+
+namespace Wpf_Client.view
+{
+    /// <summary>
+    /// Interaction logic for SinglePlayerMenu.xaml
+    /// </summary>
+    public partial class SinglePlayerWindow : Window
+    {
+        private SinglePlayerVm vm;
+        public SinglePlayerWindow()
+        {
+            InitializeComponent();
+            vm = new SinglePlayerVm(new SinglePlayerModel());
+            this.DataContext = vm;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.vm.StartGame(palr)
+        }
+    }
+}
