ad8f33d790d5449367d99ca650eff009f403ae30
hanani <hananicorney1@gmail.com>
2017-05-18 23:54:44 +0300

    merging my view model

--- ClassLibrary1/Wpf Client/viewModel/SinglePlayerVm.cs
+++ ClassLibrary1/Wpf Client/viewModel/SinglePlayerVm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Wpf_Client.model;
@@ -14,5 +15,30 @@ namespace Wpf_Client.viewModel
         {
             this.model = m;
         }
+
+        public String NameOfMaze
+        {
+            get { return model.NameOfMaze; }
+            set { model.NameOfMaze = value; }
+        }
+
+        public String NumOfRows
+        {
+            get { return model.NumOfRows; }
+            set { model.NumOfRows = value; }
+        }
+
+        public String NumOfColumns
+        {
+            get { return model.NumOfColumns; }
+            set { model.NumOfColumns = value; }
+        }
+
+        public void StartGame()
+        {
+            model.StartGame();
+        }
+
+
     }
 }
