1fffac3cbde6d8fbe7a83a6e36f9d133b70a6146
yair abramovits <yair.abramovits@live.biu.ac.il>
2017-05-19 00:15:20 +0300

    single player model

--- ClassLibrary1/Wpf Client/model/SinglePlayerModel.cs
+++ ClassLibrary1/Wpf Client/model/SinglePlayerModel.cs
@@ -1,16 +1,25 @@
 ﻿using System.ComponentModel;
+using System.Net.Mime;
 using ClientConsole;
 using MazeLib;
 
+//yaiur
 namespace WpfMaze.model 
 {
     class SinglePlayerModel : GameModel
     {
         public event PropertyChangedEventHandler PropertyChanged;
         private Maze maze;
+
         private string solution;
 
         private Client client;
+
+        private string mazeName;
+
+        private int rows;
+
+        private int cols;
 
         public SinglePlayerModel()
         {
@@ -19,14 +28,16 @@ namespace WpfMaze.model
             client.Notify += HandleStart;
         }
          
-        public void StartGame(string generate)
+        public void StartGame()
         {
+            string generate = this.mazeName + " " + this.rows + " " + this.cols;
             client.SendCommands(generate);
 
         }
         public void SolveGame()
         {
-            int algorithm = System;
+            int algorithm = Wpf_Client.Properties.Settings.Default.SearchAlgorithm;
+            string solve = "solve " + maze.Name + " " + algorithm;
             client.SendCommands(solve);
         }
 
@@ -36,11 +47,11 @@ namespace WpfMaze.model
                 PropertyChanged(this, new PropertyChangedEventArgs(name));
         }
 
-        private void HandleStart(string s)
+        private void HandleStart(string mazeString)
         {
             client.Notify -= HandleStart;
             client.Notify += HandeSolve;
-            this.maze = Maze.FromJSON(s);
+            this.maze = Maze.FromJSON(mazeString);
         }
 
         private void HandeSolve(string s)
@@ -58,19 +69,22 @@ namespace WpfMaze.model
             }
         }
 
-        public int rowProp
+        public int NumOfRows
         {
             get { return maze.Rows; }
+            set { this.rows = value; }
         }
 
-        public int colProp
+        public int NumOfCol
         {
             get { return maze.Cols; }
+            set { this.cols = value; }
         }
 
-        public string nameProp
+        public string NameOfMaze
         {
             get { return maze.Name; }
+            set { this.mazeName = value; }
         }
     }
 }
