83bcec5d2bb8bf61d447b77eb2e366882f159457
yair abramovits <yair.abramovits@live.biu.ac.il>
2017-05-19 00:35:16 +0300

    again//

---
+++ ClassLibrary1/Wpf Client/view/Single PlayerWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpf_Client.view
+{
+    /// <summary>
+    /// Interaction logic for Single_PlayerWindow.xaml
+    /// </summary>
+    public partial class Single_PlayerWindow : Window
+    {
+        public Single_PlayerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mazeBoard_Loaded(object sender, RoutedEventArgs e)
+        {
+            mazeBoard.DrawMaze();
+            //
+        }
+    }
+}
